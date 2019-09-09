a1ed2d791c367af3ca8b77fba5a0263f25b1bbbe
Aayush <joshiaayushraj@gmail.com>
2019-09-09 18:21:31 +0545

    assignment

--- jquery.js
+++ jquery.js
@@ -1,61 +1,71 @@
 $(document).ready(function() {
-  var i = 1;
-  $("#add_row").click(function() {
-    b = i - 1;
-    $("#addr" + i)
-      .html($("#addr" + b).html())
-      .find("td:first-child")
-      .html(i + 1);
-    $("#tab_logic").append('<tr id="addr' + (i + 1) + '"></tr>');
-    i++;
+  $("#add_row").on("click", function() {
+    $("#mainBody").append($("#template-product-line").html());
   });
-  $("#delete_row").click(function() {
-    if (i > 1) {
-      $("#addr" + (i - 1)).html("");
-      i--;
-    }
+  ////////////////////////////RemoveRow///////////////////
+  $("tbody").on("click", ".remove", function() {
+    $(this)
+      .closest("tr")
+      .remove();
     calc();
   });
 
+  //////////////////////////////////////////////////////
   $("#tab_logic tbody").on("keyup change", function() {
     calc();
   });
-  $("#tax").on("keyup change", function() {
-    calc_total();
+
+  /////////////////////Product--gives--->Price/////////////
+  var selectedVal = 0;
+  $("tbody").on("change", ".product", function() {
+    selectedVal = $(this)
+      .find("option:selected")
+      .data("price");
+    //point the table row of this element and then it child price
+    $(this)
+      .closest("tr")
+      .find(".price")
+      .val(selectedVal);
+
+    //point all element with class price
+    //   $(".price").val(selectedVal);
+    console.log(selectedVal);
   });
+
+  ////////////////////////Generate//////////////////////////////////
+  $("#finish").on("click", function() {
+    finish();
+  });
+
+  ////////////////////////////////////////////////////////////////
 });
 
-// $("select.form-control").on("change", function() {
-//   var item = $(this)
-//     .find(":selected")
-//     .text();
-//   switch (item) {
-//     case "Table Tennis Racket":
-//       $("input.form-control price").val(800);
-//       break;
-//     case "Badminton Racket":
-//       $("input.form-control price").val(1500);
-//       break;
-//     case "Football":
-//       $("input.form-control price").val(1200);
-//       break;
-//     case "Basketball":
-//       $("input.form-control price").val(1100);
-//       break;
-//     case "Football Gloves":
-//       $("input.form-control price").val(1900);
-//       break;
-//     case "Football Shoes":
-//       $("input.form-control price").val(2100);
-//       break;
-//     case "Chess":
-//       $("input.form-control price").val(350);
-//       break;
-//     case "ChessCarrom Board":
-//       $("input.form-control price").val(1400);
-//       break;
-//   }
-// });
+/////////////////////////////Functions////////////////////
+var purchaseItem = new Array();
+function calc_total() {
+  total = 0;
+  $(".total").each(function() {
+    total += parseInt($(this).val());
+  });
+  $("#sub_total").val(total.toFixed(2));
+  tax_sum = taxes(total);
+  $("#tax_amount").val(tax_sum.toFixed(2));
+  $("#total_amount").val((total - tax_sum).toFixed(2));
+}
+
+function taxes(income) {
+  var tax = 0;
+  if (income <= 3000) {
+    tax = 0;
+  }
+  if (income > 3000 && income <= 6000) {
+    tax = ((income - 3000) * 10) / 100;
+  }
+  if (income > 6000) {
+    tax = 300 + ((income - 6000) * 20) / 100;
+  }
+  return tax;
+}
 
 function calc() {
   $("#tab_logic tbody tr").each(function(i, element) {
@@ -76,26 +86,45 @@ function calc() {
   });
 }
 
-function calc_total() {
-  total = 0;
-  $(".total").each(function() {
-    total += parseInt($(this).val());
-  });
-  $("#sub_total").val(total.toFixed(2));
-  tax_sum = taxes(total);
-  $("#tax_amount").val(tax_sum.toFixed(2));
-  $("#total_amount").val((total - tax_sum).toFixed(2));
-}
+function finish() {
+  var temp = "";
+  $("tbody tr.newRow").each(function() {
+    var product = $(this)
+      .find(".product")
+      .val();
+    var qty = $(this)
+      .find(".qty")
+      .val();
+    var price = $(this)
+      .find(".price")
+      .val();
+    var total = qty * price;
 
-function taxes(income) {
-  if (income <= 3000) {
-    tax = 0;
-  }
-  if (income > 3000 && income <= 6000) {
-    tax = ((income - 3000) * 10) / 100;
-  }
-  if (income > 6000) {
-    tax = 300 + ((income - 6000) * 20) / 100;
-  }
-  return tax;
+    purchaseItem.push({
+      name: product,
+      Quantity: qty,
+      Rate: price,
+      Total: total
+    });
+    temp =
+      "{" +
+      "\n" +
+      "product_name :" +
+      product +
+      "," +
+      "\n" +
+      "quantity :" +
+      qty +
+      "," +
+      "\n" +
+      "unit_price :" +
+      price +
+      "," +
+      "\n" +
+      "total_price :" +
+      total +
+      "\n" +
+      "},";
+    $("#p").append($(this).text(temp));
+  });
 }
